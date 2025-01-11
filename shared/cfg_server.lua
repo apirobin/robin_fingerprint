b13ce924cda9308bf1bfa9b8f098bf7fd5bcb591
Robin.176 <75989816+apirobin@users.noreply.github.com>
2025-01-11 12:43:02 +0100

    Add files via upload
    Edited Configs

--- shared/cfg_server.lua
+++ shared/cfg_server.lua
@@ -9,8 +9,8 @@ Config.RequireJob = true
 
 -- Discord Logs
 Config.DiscordLogs = {
-    PublicLogWebhook = "https://discord.com/api/webhooks/1327587612105637888/nPxAzYm9xdxsr8gNHIsZxWBfkKSNP6uVaKcv2Unsf8rx41-ylmC-QtlZq5SYuUl4dA8L", -- Public-Log webhook
-    AdminLogWebhook = "https://discord.com/api/webhooks/1327587612105637888/nPxAzYm9xdxsr8gNHIsZxWBfkKSNP6uVaKcv2Unsf8rx41-ylmC-QtlZq5SYuUl4dA8L", -- Admin-Log webhook
+    PublicLogWebhook = "", -- Public-Log webhook
+    AdminLogWebhook = "", -- Admin-Log webhook
     SuccessColor = 3447003, -- Color for Success
     ErrorColor = 15158332, -- Color for Failure
     Texts = {
